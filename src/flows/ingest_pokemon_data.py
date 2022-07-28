9bc801ac61a7e877fbd89d3230f9a64ef93be29f
Josh Wang <hello@wangjoshuah.com>
2022-07-28 11:44:19 -0700

    finish flow for ingesting data

--- src/flows/ingest_pokemon_data.py
+++ src/flows/ingest_pokemon_data.py
@@ -1,6 +1,10 @@
+from typing import List
+
+import pandas as pd
 import httpx
 from prefect import flow,  task
-import pokebase as pb
+from prefect_dask import DaskTaskRunner
+from prefect import get_run_logger
 
 base_api = "https://pokeapi.co/api/v2/"
 
@@ -10,13 +14,20 @@ def get_all_pokemon():
     """
     Get all pokemon data from the pokeapi.co API
     """
-    return ["ditto"]
+    raw_json = httpx.get(base_api + "pokemon"
+                     , params={
+                            "limit": 151
+                            , "offset": 0
+        }).json()
+    names = [pokemon["name"] for pokemon in raw_json["results"]]
+    return names
 
 @task
 def get_pokemon_data(pokemon):
     """
     Get pokemon data from the pokeapi.co API
     """
+    print(f"Pokemon id: {pokemon}")
     return httpx.get(base_api + "pokemon/" + pokemon).json()
 
 @task
@@ -24,7 +35,20 @@ def get_pokemon_type(pokemon_data):
     """
     Get pokemon type from the pokeapi.co API
     """
-    return pokemon_data["types"]
+    name = pokemon_data["name"]
+    primary_type = pokemon_data["types"][0]["type"]["name"]
+    return {"name": name, "primary_type": primary_type}
+
+
+@flow(name="Download Pokemon Data", task_runner=DaskTaskRunner())
+def download_pokemon_data(pokemon_names: List[str]):
+    """
+    Download pokemon data from the pokeapi.co API
+    """
+    pokemon_data = [get_pokemon_data(pokemon) for pokemon in pokemon_names]
+    pokemon_types = [get_pokemon_type(data) for data in pokemon_data]
+    type_df = pd.DataFrame(pokemon_types)
+    type_df.to_csv("pokemon_data/pokemon_types.csv", index=False)
 
 @flow
 def ingest_pokemon_data():
@@ -32,9 +56,8 @@ def ingest_pokemon_data():
     Ingest all Pokemon data from the pokeapi.co API
     """
     pokemon = get_all_pokemon()
-    pokemon_data = [get_pokemon_data(pokemon) for pokemon in pokemon]
-    pokemon_types = [get_pokemon_type(data) for data in pokemon_data]
-    print(pokemon_types)
+    download_pokemon_data(pokemon)
+
 
 
 if __name__ == "__main__":
