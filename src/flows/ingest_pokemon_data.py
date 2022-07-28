e0bf9962f71151353391adbdab2c3287f29d64cb
Josh Wang <hello@wangjoshuah.com>
2022-07-28 14:40:02 -0700

    use retries and caching

--- src/flows/ingest_pokemon_data.py
+++ src/flows/ingest_pokemon_data.py
@@ -2,11 +2,13 @@ from typing import List
 
 import pandas as pd
 import httpx
-from prefect import flow,  task
+from prefect import flow, task
 from prefect_dask import DaskTaskRunner
-from prefect import get_run_logger
+from prefect.tasks import task_input_hash
+import json
 
 base_api = "https://pokeapi.co/api/v2/"
+_storage_base_dir = "./pokemon_data/"
 
 
 @task
@@ -14,23 +16,28 @@ def get_all_pokemon():
     """
     Get all pokemon data from the pokeapi.co API
     """
-    raw_json = httpx.get(base_api + "pokemon"
-                     , params={
-                            "limit": 151
-                            , "offset": 0
-        }).json()
+    raw_json = httpx.get(
+        base_api + "pokemon", params={"limit": 151, "offset": 0}
+    ).json()
     names = [pokemon["name"] for pokemon in raw_json["results"]]
+    # names = ["ditto"]
     return names
 
-@task
+
+@task(cache_key_fn=task_input_hash, retries=3)
 def get_pokemon_data(pokemon):
     """
     Get pokemon data from the pokeapi.co API
     """
     print(f"Pokemon id: {pokemon}")
-    return httpx.get(base_api + "pokemon/" + pokemon).json()
+    data = httpx.get(base_api + "pokemon/" + pokemon).json()
+    f = open(_storage_base_dir + pokemon + ".json", "w")
+    f.write(json.dumps(data))
+    f.close()
+    return data
 
-@task
+
+@task(cache_key_fn=task_input_hash, retries=3)
 def get_pokemon_type(pokemon_data):
     """
     Get pokemon type from the pokeapi.co API
@@ -40,24 +47,33 @@ def get_pokemon_type(pokemon_data):
     return {"name": name, "primary_type": primary_type}
 
 
+@task()
+def output_pokemon_types(pokemon_types):
+    """
+    Output pokemon types to a CSV file
+    """
+    df = pd.DataFrame(pokemon_types)
+    df.to_csv(_storage_base_dir + "pokemon_types.csv", index=False)
+
+
 @flow(name="Download Pokemon Data", task_runner=DaskTaskRunner())
 def download_pokemon_data(pokemon_names: List[str]):
     """
     Download pokemon data from the pokeapi.co API
     """
-    pokemon_data = [get_pokemon_data(pokemon) for pokemon in pokemon_names]
-    pokemon_types = [get_pokemon_type(data) for data in pokemon_data]
-    type_df = pd.DataFrame(pokemon_types)
-    type_df.to_csv("pokemon_data/pokemon_types.csv", index=False)
+    pokemon_data = [get_pokemon_data.submit(pokemon) for pokemon in pokemon_names]
+    pokemon_types = [get_pokemon_type.submit(data) for data in pokemon_data]
+    return pokemon_types
+
 
 @flow
 def ingest_pokemon_data():
     """
     Ingest all Pokemon data from the pokeapi.co API
     """
-    pokemon = get_all_pokemon()
-    download_pokemon_data(pokemon)
-
+    pokemon = get_all_pokemon.submit()
+    pokemon_types = download_pokemon_data(pokemon)
+    output_pokemon_types.submit(pokemon_types)
 
 
 if __name__ == "__main__":
